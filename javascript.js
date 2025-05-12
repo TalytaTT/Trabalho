e6a82cf086e50dc5b3231d63760f781356c244f8
TalytaTT <talytavieirafaculdade@gmail.com>
2025-05-12 17:46:54 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,3 @@
+function exibirMensagem() {
+    document.getElementById("mensagem").innerText = "Sempre codifique como se o cara que fosse manter seu código fosse um psicopata violento que sabe onde você mora. – Martin Golding";
+}
